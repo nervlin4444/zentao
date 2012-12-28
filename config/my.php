13fe666d21a378022d551c284a3b6385e9cb615e
nervlin <nerv_lin@yahoo.com.hk>
2012-12-29 01:31:55 +0800

    linux version unique commit in 29-12-2012

---
+++ config/my.php
@@ -0,0 +1,13 @@
+<?php
+$config->installed       = true;
+$config->debug           = false;
+$config->requestType     = 'PATH_INFO';
+$config->db->host        = 'localhost';
+$config->db->port        = '3306';
+$config->db->name        = 'zentao';
+$config->db->user        = 'root';
+$config->db->password    = '4444';
+$config->db->prefix      = 'zt_';
+$config->webRoot         = '/';
+$config->default->domain = 'yii.zapto.org';
+$config->default->lang   = 'zh-tw';
---
+++ module/mail/ext/config/zzzemail.php
@@ -0,0 +1,12 @@
+<?php
+$config->mail->turnon         = 1;
+$config->mail->mta            = 'smtp';
+$config->mail->fromAddress    = "kevinlam@kwiksure.com"; 
+$config->mail->fromName       = "zentao";
+$config->mail->smtp->host     = "mail.kiwisure.com";
+$config->mail->smtp->port     = "25";
+$config->mail->smtp->auth     = 1;
+$config->mail->smtp->username = "kevinlam@kwiksure.com";
+$config->mail->smtp->password = "kevinlam7gs";
+$config->mail->smtp->secure   = "";
+$config->mail->smtp->debug    = 1;
