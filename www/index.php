25c0bff7efcc4722e4fd9df00b8062403830cb87
nervlin <nerv_lin@yahoo.com.hk>
2013-01-01 15:56:10 +0800

    commit 2013

--- www/index.php
+++ www/index.php
@@ -40,4 +40,28 @@ $common->checkPriv();
 $app->loadModule();
 
 /* Flush the buffer. */
-ob_end_flush();
+//ob_end_flush();
+echo preg_replace("/\<\/body\>[\s]*\<\/html\>/","",ob_get_clean());
+?>
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-37268378-6']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+<script type="text/javascript">
+document.write(unescape("%3Cscript src='http://near.zapto.org/piwik.js' type='text/javascript'%3E%3C/script%3E"));
+</script><script type="text/javascript">
+try {
+var piwikTracker = Piwik.getTracker("http://near.zapto.org/piwik.php", 5);
+piwikTracker.trackPageView();
+piwikTracker.enableLinkTracking();
+} catch( err ) {}
+</script><noscript><p><img src="http://near.zapto.org/piwik.php?idsite=5" style="border:0" alt="" /></p></noscript>
+<?php 
+echo "</body></html>";
+?>
